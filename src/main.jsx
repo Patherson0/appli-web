6d2452a4ed14512e9ea326c7c941ddb75fb11a64
Patherson0 <126504452+Patherson0@users.noreply.github.com>
2026-03-23 17:22:01 +0100

    Create React entry point file

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to the Application!</h1>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
