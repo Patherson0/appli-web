d000d61e656364b6f62bbe68bea6c130b4e12b69
Patherson0 <126504452+Patherson0@users.noreply.github.com>
2026-03-23 17:41:26 +0100

    Create TaskList component

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import TaskItem from './TaskItem';
+
+const TaskList = ({ tasks, deleteTask, toggleTask }) => {
+    return (
+        <div>
+            <h2>Task List</h2>
+            <ul>
+                {tasks.map(task => (
+                    <TaskItem 
+                        key={task.id} 
+                        task={task} 
+                        deleteTask={deleteTask} 
+                        toggleTask={toggleTask} 
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TaskList;
